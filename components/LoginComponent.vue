7512a8b910c9fb8cb4522447054f2ebffdae9bf2
Nathan Lamont <nathan@bigggerplanet.com>
2023-08-20 10:12:31 -0400

    use previous versions of prettier etc.

--- components/LoginComponent.vue
+++ components/LoginComponent.vue
@@ -1,28 +1,28 @@
 <template>
-  <form @submit.prevent="login" class="flex flex-col">
+  <form class="flex flex-col" @submit.prevent="login">
     <label>
-        <span class="block">Username</span>
-              <input v-model="email" class="w-full" />      
+      <span class="block">Username</span>
+      <input v-model="email" class="w-full" />
     </label>
-              <label for="password"><span class="block">Password</span>
-              <input id="password" v-model="password" class="w-full" />
-            </label>
-            <button type="submit">Log In!</button>
-      </form>
-
+    <label for="password"
+      ><span class="block">Password</span>
+      <input id="password" v-model="password" class="w-full" />
+    </label>
+    <button type="submit">Log In</button>
+  </form>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios';
 
-const m = import.meta.env.VITE_API_URL
-const email = ref('')
-const password = ref('')
+const m = import.meta.env.VITE_API_URL;
+const email = ref('');
+const password = ref('');
 
 function login() {
   axios.post(m + '/auth/login', {
     email: email.value,
-    password: password.value
-  })
+    password: password.value,
+  });
 }
 </script>
